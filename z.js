d9199bd773a168ddc39ce4f654648e6e66beedc0
zerowolfcode <33560393+zerowolfcode@users.noreply.github.com>
2017-11-10 22:51:06 -0700

    Update z.js

--- z.js
+++ z.js
@@ -1,28 +1,29 @@
 function z(id){
- var about = {
-  Version: 0.1,
-  Author: 'ZWC',
-  Updated: '20171108'
- };
- if(id){
-  if(window === this){
-   return new z(id);
-  }
-  this.e=document.querySelectorAll(id);
-  return this;
- }else{
- return about;
- }
+ if(window===this){return new z(id)}
+ this.e=document.querySelectorAll(id);
+ return this;
 };
-z.prototype = {
- h: function(){
+z.prototype={
+ h:function(){
   for(var i=0;i<this.e.length;i++){
-   this.e[i].style.display = 'none';
+   this.e[i].style.display='none';
   }
   return this;
  },
- s: function(){
-  this.e.style.display = 'inherit';
+ s:function(){
+  for(var i=0;i<this.e.length;i++){
+   this.e[i].style.display='';
+  }
   return this;
+ },
+ c:function(e){return document.createElement(e)},
+ a:function(e){this.e[0].appendChild(e)},
+ html:function(html){
+  if(!html){html=this.e[0].innerHTML};
+  return this.e[0].innerHTML=html;
+ },
+ v:function(v){
+  if(!v){v=this.e[0].value};
+  return this.e[0].value=v;
  }
 }
