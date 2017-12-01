2ada88327efa8b3afb63b513b1222e0e4228c82f
zerowolfcode <33560393+zerowolfcode@users.noreply.github.com>
2017-11-30 17:55:58 -0700

    Update z.js

--- z.js
+++ z.js
@@ -1,17 +1,16 @@
 var z=(function(z){
- z.z=function(z){
-  if(z.tagName){return [z]};
-  return document.querySelectorAll(z);
- };
- return z;
-}(z||{}));
-var z=(function(z){
+ z.z=function(z){if(z.tagName){return [z]};return document.querySelectorAll(z);};
  z.h=function(e){e=z.z(e);for(var i=0;i<e.length;i++){e[i].style.display='none'}};
  z.s=function(e){e=z.z(e);for(var i=0;i<e.length;i++){e[i].style.display=''}};
  z.html=function(e,html){
   e=z.z(e)[0];
-  if(!html){html=e.innerHTML}
+  if(!html){return e.innerHTML}
   return e.innerHTML=html;
+ };
+ z.txt=function(e,t){
+  e=z.z(e)[0];
+  if(!t){return e.textContent}
+  return e.textContent=t;
  };
  z.v=function(e,v){
   e=z.z(e)[0];
