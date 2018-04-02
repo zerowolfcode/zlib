89da4fca9684c282af8c241ca2206179f5670cf9
zerowolfcode <33560393+zerowolfcode@users.noreply.github.com>
2018-04-01 20:57:46 -0600

    Update zt.js

--- zt.js
+++ zt.js
@@ -1,8 +1,3 @@
-function z(z){if(z.tagName){return [z]};return document.querySelectorAll(z);}
-function each(e,it){e=z.z(e);for(var i=0;i<e.length;i++){it(e[i],i,e);}}
-function h(e){z.each(e,function(e){e.style.display='none'});}
-module.exports = {
-  z:z,
-  each:each,
-  h:h,
-};
+export function z(z){if(z.tagName){return [z]};return document.querySelectorAll(z);}
+export function each(e,it){e=z.z(e);for(var i=0;i<e.length;i++){it(e[i],i,e);}}
+export function h(e){z.each(e,function(e){e.style.display='none'});}
