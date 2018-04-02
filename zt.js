7ad751570c2520febaa0b5b44e937e9c60ae80d8
zerowolfcode <33560393+zerowolfcode@users.noreply.github.com>
2018-04-01 21:42:42 -0600

    Update zt.js

--- zt.js
+++ zt.js
@@ -1,6 +1,11 @@
+/*
 function z(z){if(z.tagName){return [z]};return document.querySelectorAll(z);}
 function each(e,it){e=z(e);for(var i=0;i<e.length;i++){it(e[i],i,e);}}
 function h(e){each(e,function(e){e.style.display='none'});}
+*/
+var z=function(z){if(z.tagName){return [z]};return document.querySelectorAll(z);},
+each=function(e,it){e=z(e);for(var i=0;i<e.length;i++){it(e[i],i,e);}},
+h=function(e){each(e,function(e){e.style.display='none'});};
 export {z,each,h}
 /*
 export function z(z){if(z.tagName){return [z]};return document.querySelectorAll(z);}
