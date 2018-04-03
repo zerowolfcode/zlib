7a933a255c849b065078bc9ffabd4b134df9dd51
zerowolfcode <33560393+zerowolfcode@users.noreply.github.com>
2018-04-03 16:52:37 -0600

    Update zt.js

--- zt.js
+++ zt.js
@@ -1,30 +1,14 @@
-export var z=z=>{if(z.tagName)return [z];return document.querySelectorAll(z);},
+export var d=document,
+z=(e,n)=>{
+  if(e.tagName)e=[e];
+  if(typeof e=='string')e=d.querySelectorAll(e);
+  return e=n==''?e[n]:e;
+},
 each=(e,it)=>{e=z(e);for(var i=0;i<e.length;i++){it(e[i],i,e);}},
 h=e=>each(e,e=>e.style.display='none'),
 s=e=>each(e,e=>e.style.display=''),
-html=(e,html)=>{
-  e=z(e)[0];
-  if(!html)return e.innerHTML;
-  return e.innerHTML=html;
-},
-txt=(e,t)=>{
-  e=z(e)[0];
-  if(!t)return e.textContent;
-  return e.textContent=t;
-},
-v=(e,v)=>{
-  e=z(e)[0];
-  if(!v)return e.value;
-  return e.value=v;
-},
-c=e=>document.createElement(e),
-a=(p,e)=>{z(p)[0].appendChild(e);return p},
-child=(e,c)=>{
-  e=z(e)[0];
-  if(Array.isArray(c)){
-    e=e.children[c[0]];
-    var i=1;
-    while(i<c.length){e=e.children[c[i++]]}
-  }else{e=e.children[c]}
-  return e;
-};
+html=(e,html)=>html===undefined?z(e,0).innerHTML:z(e,0).innerHTML=html,
+txt=(e,t)=>t===undefined?z(e,0).textContent:z(e,0).textContent=t,
+v:(e,v)=>v===undefined?z(e,0).value:z(e,0).value=v,
+c=e=>d.createElement(e),
+a=(p,e)=>{z(p,0).appendChild(e);return p},
