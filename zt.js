741d705fa3d7e27b617e0ba6f1093e38f855ced5
zerowolfcode <33560393+zerowolfcode@users.noreply.github.com>
2018-06-10 19:15:27 -0600

    Update zt.js

--- zt.js
+++ zt.js
@@ -1,15 +1,2 @@
 export var d=document,
-z=(e,n)=>{
-  if(e.tagName)e=[e];
-  if(typeof e=='string')e=d.querySelectorAll(e);
-  return e=n==''?e[n]:e;
-},
-each=(e,it)=>{e=z(e);for(var i=0;i<e.length;i++){it(e[i],i,e);}},
-h=e=>each(e,e=>e.style.display='none'),
-s=e=>each(e,e=>e.style.display=''),
-html=(e,html)=>html===undefined?z(e,0).innerHTML:z(e,0).innerHTML=html,
-txt=(e,t)=>t===undefined?z(e,0).textContent:z(e,0).textContent=t,
-v=(e,v)=>v===undefined?z(e,0).value:z(e,0).value=v,
-c=e=>d.createElement(e),
-a=(p,e)=>{z(p,0).appendChild(e);return p},
-clk=(e,f)=>{z(e,0).onclick=f};
+txt=(e,t)=>d.getElementById(e).textContent=t,
